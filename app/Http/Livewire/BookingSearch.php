e4c555e4ce824589546e4b8cf4c041ea69643ed7
Yoel Baez <jbaezgis@gmail.com>
2022-09-25 08:54:36 -0400

    adding classes and migrations - 1.0.0.9

---
+++ app/Http/Livewire/BookingSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BookingSearch extends Component
+{
+    public function render()
+    {
+        return view('livewire.booking-search')->layout('layouts.guest')s;
+    }
+}
